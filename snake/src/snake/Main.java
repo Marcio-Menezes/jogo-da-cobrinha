f9ae960d5be3837e870f2b2426d8d8e811950e04
Marcio-Menezes <81115083+Marcio-Menezes@users.noreply.github.com>
2023-05-21 22:52:56 -0300

    Update Main.java

--- snake/src/snake/Main.java
+++ snake/src/snake/Main.java
@@ -4,16 +4,13 @@ package snake;
  *
  * @author Marcio Menezes
  */
-import java.awt.Font;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        // Executador do game, evite mexer aqui caso não saiba o que está fazendo
         Game game = new Game();
-        JLabel pontuacao;
         JFrame frame = new JFrame("Jogo da Cobrinha");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(game);
